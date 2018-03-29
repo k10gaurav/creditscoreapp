a0b93aeaefb3ca4252a100fd8edef781701beb27
kumar.gaurav <kumar.gaurav@gen-xt.com>
2018-03-29 17:03:15 +0530

    modified JS for score calculation

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -7,12 +7,27 @@ $(function() {
 		$("#status").removeClass('status-ok');
 		
         e.preventDefault();
-		
+		/*
 		var dt=$("#date").val();
 		var mth=$("#month").val();
 		var yr=$("#year").val();
 		var score=((dt+mth+Math.floor(yr/100))/2);
+		*/
+		var score=getRandomInt(500,1000);
 		$("#status").addClass("status-ok").html("CREDIT SCORE IS: "+(score));
       
     });
 });
+
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
